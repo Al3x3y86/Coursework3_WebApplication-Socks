58a57d0b571b75197d287cd83123bc4a56c6b4fb
Alexey <alexey_kuzminykh@mail.ru>
2022-12-20 12:46:14 +0300

    initial commit

---
+++ src/main/java/com/socks/coursework3_webapplicationsocks/Coursework3WebApplicationSocksApplication.java
@@ -0,0 +1,13 @@
+package com.socks.coursework3_webapplicationsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3WebApplicationSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework3WebApplicationSocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socks/coursework3_webapplicationsocks/Coursework3WebApplicationSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socks.coursework3_webapplicationsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework3WebApplicationSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
